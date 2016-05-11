ad45c65ca8a86649fa23631b13db32639ec12389
brunosanchez <bruno@oac.unc.edu.ar>
2016-05-11 12:38:15 -0300

    Started with coadd

---
+++ propercoadd.py
@@ -0,0 +1,12 @@
+"""
+sim_coadd_proper translation to Python.
+
+Written by Bruno SANCHEZ
+
+PhD of Astromoy - UNC"""
+
+import numpy as np
+import scipy.fftpack as fft
+
+at this point we assume two images.
+
