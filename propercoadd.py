d4c408203689ce11c4eeffd141a397ec79d359d2
brunosanchez <bruno@oac.unc.edu.ar>
2016-05-13 17:11:29 -0300

    Several changes

--- propercoadd.py
+++ propercoadd.py
@@ -5,8 +5,17 @@ Written by Bruno SANCHEZ
 
 PhD of Astromoy - UNC"""
 
+
 import numpy as np
 import scipy.fftpack as fft
+from imageSimulation import big_code
 
-at this point we assume two images.
+# at this point we assume ten images.
+# An image is a background substracted image,
+#  with a psf and a flux scale, plus some random gaussian noise.
+
+#  M_j = (F_j T) * Psf_j + e_j
+
+# and we have an idea of Var(e_j) = sigma_j (assuming...)
+
 
---
+++ test_proper_coadd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 17:06:14 2016
+
+@author: bruno
+"""
+
+from imageSimulation import big_code
+
+N = 512  # side
+
+m = big_code.delta_point(N)
+
+im = big_code.image(m, N, t_exp=1, FWHM=10, SN=100)
+
