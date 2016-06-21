73dfce8090bbfa75e20eda7d6d34d2923c083a91
brunosanchez <bruno@oac.unc.edu.ar>
2016-06-21 14:05:22 -0300

    testing S reconstruction using real images

---
+++ test_real_coadd.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  test_recoverstats.py
+#
+#  Copyright 2016 Bruno S <bruno.sanchez.63@gmail.com>
+#
+
+import os
+import shlex
+import subprocess
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from astropy.io import fits
+
+import propercoadd as pc
+
+# =============================================================================
+#     PSF measure test by propercoadd
+# =============================================================================
+datapath = os.path.abspath('/home/bruno/Documentos/reduccionTolar/20151212/Landolt_C53')
+
+S = np.zeros((1365, 1365))
+
+for root, dirs, files in os.walk(datapath):
+    for afile in files:
+        frame = os.path.join(root, afile)
+        sim = pc.SingleImage(frame, imagefile=True)
+        s_comp = sim.s_component()
+
+        S += s_comp
+
+test_dir = os.path.abspath('./test_images/real_coadd_test/')
+
+plt.figure(figsize=(16,16))
+plt.imshow(np.log10(S), interpolation='none')
+plt.colorbar(orientation='horizontal')
+plt.savefig(os.path.join(test_dir, 'S.png'))
+plt.close()
+
+
+
