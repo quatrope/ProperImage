db75586456054a6b9780e3a7c627af29c1625549
brunosanchez <bruno@oac.unc.edu.ar>
2016-08-09 01:07:29 -0300

    Now setup.py is able to perform install

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+from setuptools import find_packages
 from setuptools import setup
 
 setup(name='properimage',
-      version='0.1',
+      version='0.1.0.dev1',
       description='Proper astronomic image analysis',
       author='Bruno Sanchez',
       author_email='bruno@oac.unc.edu.ar',
       url='https://github.com/toros-astro/ProperImage',
-      py_modules=['properimage', ],
+      py_modules=['propercoadd', 'imsim'],
       install_requires=["numpy>=1.6.2",
                         "scipy>=0.15",
                         "astropy>=1.0",
-                        ],
-      test_suite='tests',
+                        ] ,
+      packages=find_packages(exclude=['tests*'])
+      # test_suite='tests',
 )
