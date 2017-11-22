c4db75331cc96f509a7bb33ce49e595488a455a0
brunosanchez <bruno@oac.unc.edu.ar>
2017-11-22 14:17:17 -0300

    New test

---
+++ test/test_subtract_sniptf.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  test_subtract_sniptf.py
+#
+#  Copyright 2017 Bruno S <bruno@oac.unc.edu.ar>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+import glob
+import os
+from properimage import single_image as s
+from properimage import propersubtract2 as ps
+from astropy.io import fits
+from properimage import utils
+from properimage import plot
+
+
+def main(args):
+    imgsdir = '/home/bruno/Data/SNiPTF/imgs'
+    imgsdir = '/home/bruno/Documentos/Data/SNiPTF/imgs'
+
+    #imgsdir = '/home/bruno/Documentos/Data/LIGO_O2/20171116/ESO202-009'
+    dest_dir = './test/test_images/test_sub_sniptf'
+    imgs = glob.glob(imgsdir+'/*sci*.fits')
+    mask = glob.glob(imgsdir+'/*mask*.fits')
+
+    imgs.sort()
+    mask.sort()
+
+    images = [s.SingleImage(animg, mask=amask) for animg, amask in zip(imgs, mask)]
+
+    #images = [s.SingleImage(animg) for animg in imgs]
+
+    D, P, S_corr = ps.diff(images[0], images[1], align=False, iterative=False, shift=False, beta=True)
+
+
+    fits.writeto(os.path.join(dest_dir,'Diff1.fits'), D.real, overwrite=True)
+    fits.writeto(os.path.join(dest_dir,'P1.fits'), P.real, overwrite=True)
+    fits.writeto(os.path.join(dest_dir,'Scorr1.fits'), S_corr, overwrite=True)
+
+    for an_img in images:
+        an_img._clean()
+
+    return
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
