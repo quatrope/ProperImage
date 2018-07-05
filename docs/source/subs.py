22156fab1c11c7c775fa71c433a385f445c27535
brunosanchez <bruno@oac.unc.edu.ar>
2018-07-05 00:54:57 -0300

    Fixed some warnings on docs

---
+++ docs/source/subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+MOCK_MODULES = []
+
+
+TARGETS = {
+    "pip": "https://pip.pypa.io/",
+    "virtualenv": "http://www.virtualenv.org/",
+    "virtualenvwrapper": "https://virtualenvwrapper.readthedocs.org/en/latest/",
+    "standalone pip installer": "https://pip.pypa.io/en/latest/installing.html#install-pip",
+
+    "Git": "http://git-scm.com/",
+}
+
