e6379d224fb2db73c93f69381af76eca446d6c16
Bruno Sanchez <bruno.sanchez@duke.edu>
2022-12-28 18:20:40 -0500

    Multiple fixes and additions. Trying to launch v.8

--- setup.py
+++ setup.py
@@ -102,6 +102,8 @@ def do_setup():
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: Implementation :: CPython",
             "Topic :: Scientific/Engineering",
         ],
