9c0e7783998b93d5bd9ee144e3d4e43efac6d54d
Bruno Sanchez <bsanchez@cppm.in2p3.fr>
2025-05-22 15:06:18 +0200

    Add parallel with joblib

--- properimage/operations.py
+++ properimage/operations.py
@@ -36,6 +36,8 @@ import sep
 
 from . import utils as u
 from .single_image import SingleImage as si
+from joblib import Parallel, delayed
+import concurrent.futures
 
 try:
     import pyfftw
@@ -444,107 +446,23 @@ def diff(*args, **kwargs):
     return subtract(*args, **kwargs)
 
 
-class StackCombinator(Process):
-    """Combination engine.
-
-    An engine for image combination in parallel, using multiprocessing.Process
-    class.
-    Uses an ensemble of images and a queue to calculate the propercoadd of
-    the list of images.
-
-    Parameters
-    ----------
-    img_list: list or tuple
-        list of SingleImage instances used in the combination process
-
-    queue: multiprocessing.Queue instance
-        an instance of multiprocessing.Queue class where to pickle the
-        intermediate results.
-
-    shape: shape of the images being coadded.
-
-    stack: boolean, default True
-        Whether to stack the results for coadd or just obtain individual
-        image calculations.
-        If True it will pickle in queue a coadded image of the chunk's images.
-        If False it will pickle in queue a list of individual matched filtered
-        images.
-
-    fourier: boolean, default False.
-        Whether to calculate individual fourier transform of each s_component
-        image.
-        If stack is True this parameter will be ignored.
-        If stack is False, and fourier is True, the pickled object will be a
-        tuple of two values, with the first one containing the list of
-        s_components, and the second one containing the list of fourier
-        transformed s_components.
-
-    Returns
-    -------
-    Combinator process
-        An instance of Combinator.
-        This can be launched like a multiprocessing.Process
-
-    Example
-    -------
-    queue1 = multiprocessing.Queue()
-    queue2 = multiprocessing.Queue()
-    p1 = Combinator(list1, queue1)
-    p2 = Combinator(list2, queue2)
-
-    p1.start()
-    p2.start()
-
-    #results are in queues
-    result1 = queue1.get()
-    result2 = queue2.get()
-
-    p1.join()
-    p2.join()
-
-    """
-
-    def __init__(
-        self,
-        img_list,
-        queue,
-        shape,
-        stack=True,
-        fourier=False,
-        *args,
-        **kwargs,
-    ):
-        """Create instance of combination engine."""
-        super(StackCombinator, self).__init__(*args, **kwargs)
-        self.list_to_combine = img_list
-        self.queue = queue
-        self.global_shape = shape
-        logging.getLogger("StackCombinator").info(self.global_shape)
-        # self.zps = ensemble.transparencies
-
-    def run(self):
-        """Run workers of combination engine."""
-        S_hat = np.zeros(self.global_shape).astype(np.complex128)
-        psf_hat_sum = np.zeros(self.global_shape).astype(np.complex128)
-        mix_mask = self.list_to_combine[0].data.mask
-
-        for an_img in self.list_to_combine:
-            np.add(an_img.s_hat_comp, S_hat, out=S_hat, casting="same_kind")
-            np.add(
-                ((an_img.zp / an_img.var) ** 2) * an_img.psf_hat_sqnorm(),
-                psf_hat_sum,
-                out=psf_hat_sum,
-            )  # , casting='same_kind')
-            # psf_hat_sum = ((an_img.zp/an_img.var)**2)*an_img.psf_hat_sqnorm()
-            mix_mask = np.ma.mask_or(mix_mask, an_img.data.mask)
-
-        serialized = pickle.dumps([S_hat, psf_hat_sum, mix_mask])
-        self.queue.put(serialized)
-        return
+def process_chunk(chunk, global_shape):
+    S_hat = np.zeros(global_shape).astype(np.complex128)
+    psf_hat_sum = np.zeros(global_shape).astype(np.complex128)
+    mix_mask = chunk[0].data.mask
+    for an_img in chunk:
+        np.add(an_img.s_hat_comp, S_hat, out=S_hat, casting="same_kind")
+        np.add(
+            ((an_img.zp / an_img.var) ** 2) * an_img.psf_hat_sqnorm(),
+            psf_hat_sum,
+            out=psf_hat_sum,
+        )
+        mix_mask = np.ma.mask_or(mix_mask, an_img.data.mask)
+    return S_hat.real, S_hat.imag, psf_hat_sum.real, psf_hat_sum.imag, mix_mask
 
 
 def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
-    """Coadd a list of SingleImage instances using R estimator.
+    """Coadd a list of SingleImage instances using R estimator with concurrent.futures.
 
     Parameters:
     -----------
@@ -554,7 +472,7 @@ def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
         Value of information loss in PSF estimation, lower limit is 0,
         upper is 1. Only valid if fitted_psf=False. Default is 0.25
     n_procs : int
-        Number of processes to use. If value is one then no multiprocessing
+        Number of parallel workers to use. If value is one then no parallelism
         is being used. Default 2.
 
     Returns:
@@ -592,44 +510,152 @@ def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
     psf_shape = (psf_shape, psf_shape)
 
     if n_procs > 1:
-        queues = []
-        procs = []
-        for chunk in u.chunk_it(img_list, n_procs):
-            queue = Queue()
-            proc = StackCombinator(
-                chunk, queue, shape=global_shape, stack=True, fourier=False
-            )
-            logger.info("starting new process")
-            proc.start()
-
-            queues.append(queue)
-            procs.append(proc)
-
-        logger.info("all chunks started, and procs appended")
+        chunks = list(u.chunk_it(img_list, n_procs))
+        results = []
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=n_procs
+        ) as executor:
+            futures = [
+                executor.submit(process_chunk, chunk, global_shape)
+                for chunk in chunks
+            ]
+            for future in concurrent.futures.as_completed(futures):
+                results.append(future.result())
 
         S_hat = np.zeros(global_shape, dtype=np.complex128)
         P_hat = np.zeros(global_shape, dtype=np.complex128)
         mix_mask = np.zeros(global_shape, dtype=np.bool_)
-        for q in queues:
-            serialized = q.get()
-            logger.info("loading pickles")
-            s_hat_comp, psf_hat_sum, mask = pickle.loads(serialized)
-            np.add(s_hat_comp, S_hat, out=S_hat)  # , casting='same_kind')
-            np.add(psf_hat_sum, P_hat, out=P_hat)  # , casting='same_kind')
+        # for s_hat_comp, psf_hat_sum, mask in results:
+        for (
+            S_hat_real,
+            S_hat_imag,
+            psf_hat_sum_real,
+            psf_hat_sum_imag,
+            mask,
+        ) in results:
+            np.add(S_hat_real, S_hat.real, out=S_hat.real)
+            np.add(S_hat_imag, S_hat.imag, out=S_hat.imag)
+            np.add(psf_hat_sum_real, P_hat.real, out=P_hat.real)
+            np.add(psf_hat_sum_imag, P_hat.imag, out=P_hat.imag)
             mix_mask = np.ma.mask_or(mix_mask, mask)
 
         P_r_hat = np.sqrt(P_hat)
         P_r = _ifftwn(fourier_shift(P_r_hat, psf_shape))
         P_r = P_r / np.sum(P_r)
         R = _ifftwn(S_hat / np.sqrt(P_hat))
+    else:
+        S_hat = np.zeros(global_shape, dtype=np.complex128)
+        P_hat = np.zeros(global_shape, dtype=np.complex128)
+        mix_mask = img_list[0].data.mask
 
-        logger.info("S calculated, now starting to join processes")
+        for an_img in img_list:
+            np.add(an_img.s_hat_comp, S_hat, out=S_hat)
+            np.add(
+                ((an_img.zp / an_img.var) ** 2) * an_img.psf_hat_sqnorm(),
+                P_hat,
+                out=P_hat,
+            )
+            mix_mask = np.ma.mask_or(mix_mask, an_img.data.mask)
+        P_r_hat = np.sqrt(P_hat)
+        P_r = _ifftwn(fourier_shift(P_r_hat, psf_shape))
+        P_r = P_r / np.sum(P_r)
+        R = _ifftwn(S_hat / P_r_hat)
 
-        for proc in procs:
-            logger.info("waiting for procs to finish")
-            proc.join()
+    return R, P_r, mix_mask
 
-        logger.info("processes finished, now returning R")
+
+def process_chunk_joblib(chunk, global_shape):
+    S_hat = np.zeros(global_shape).astype(np.complex128)
+    psf_hat_sum = np.zeros(global_shape).astype(np.complex128)
+    mix_mask = chunk[0].data.mask
+
+    for an_img in chunk:
+
+        np.add(an_img.s_hat_comp, S_hat, out=S_hat, casting="same_kind")
+        np.add(
+            ((an_img.zp / an_img.var) ** 2) * an_img.psf_hat_sqnorm(),
+            psf_hat_sum,
+            out=psf_hat_sum,
+        )
+        mix_mask = np.ma.mask_or(mix_mask, an_img.data.mask)
+    return S_hat.real, S_hat.imag, psf_hat_sum.real, psf_hat_sum.imag, mix_mask
+
+
+def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
+    """Coadd a list of SingleImage instances using R estimator with joblib.
+
+    Parameters:
+    -----------
+    align : bool
+        Whether to align the images before subtracting, default to False
+    inf_loss : float
+        Value of information loss in PSF estimation, lower limit is 0,
+        upper is 1. Only valid if fitted_psf=False. Default is 0.25
+    n_jobs : int
+        Number of parallel workers to use. If value is one then no parallelism
+        is being used. Default 2.
+
+    Returns:
+    --------
+    R : np.ndarray(n, m) of float
+        Coadd image, Zackay's decorrelated R.
+    P : np.ndarray(n, m) of float
+        Coadd image PSF. This is a full PSF image, with a size equal to R
+    mix_mask : np.ndarray of bool
+        Mask of bad pixels for subtracion image, with True marking bad pixels
+    """
+    logger = logging.getLogger()
+    for i_img, animg in enumerate(si_list):
+        if not isinstance(animg, si):
+            si_list[i_img] = si(animg)
+
+    if align:
+        img_list = u._align_for_coadd(si_list)
+        for an_img in img_list:
+            an_img.update_sources()
+    else:
+        img_list = si_list
+
+    shapex = np.min([an_img.data.shape[0] for an_img in img_list])
+    shapey = np.min([an_img.data.shape[1] for an_img in img_list])
+    global_shape = (shapex, shapey)
+
+    zps, meanmags = u.transparency(img_list)
+    for j, an_img in enumerate(img_list):
+        an_img.zp = zps[j]
+        an_img._setup_kl_a_fields(inf_loss)
+
+    psf_shapes = [an_img.stamp_shape[0] for an_img in img_list]
+    psf_shape = np.max(psf_shapes)
+    psf_shape = (psf_shape, psf_shape)
+    n_jobs = n_procs
+    if n_jobs > 1:
+        chunks = list(u.chunk_it(img_list, n_jobs))
+        results = Parallel(n_jobs=n_jobs, backend="threading")(
+            delayed(process_chunk_joblib)(chunk, global_shape)
+            for chunk in chunks
+        )
+
+        S_hat = np.zeros(global_shape, dtype=np.complex128)
+        P_hat = np.zeros(global_shape, dtype=np.complex128)
+        mix_mask = np.zeros(global_shape, dtype=np.bool_)
+        for (
+            S_hat_real,
+            S_hat_imag,
+            psf_hat_sum_real,
+            psf_hat_sum_imag,
+            mask,
+        ) in results:
+            np.add(S_hat_real, S_hat.real, out=S_hat.real)
+            np.add(S_hat_imag, S_hat.imag, out=S_hat.imag)
+            np.add(psf_hat_sum_real, P_hat.real, out=P_hat.real)
+            np.add(psf_hat_sum_imag, P_hat.imag, out=P_hat.imag)
+            mix_mask = np.ma.mask_or(mix_mask, mask)
+
+        P_r_hat = np.sqrt(P_hat)
+        P_r = _ifftwn(fourier_shift(P_r_hat, psf_shape))
+        P_r = P_r / np.sum(P_r)
+        R = _ifftwn(S_hat / np.sqrt(P_hat))
     else:
         S_hat = np.zeros(global_shape, dtype=np.complex128)
         P_hat = np.zeros(global_shape, dtype=np.complex128)
