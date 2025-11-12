5d95496bdf3f1ce79bcab05532dce3d369d2e7df
Bruno Sanchez <bsanchez@cppm.in2p3.fr>
2025-11-12 13:55:30 +0100

    Fixed multiple bugs

--- properimage/operations.py
+++ properimage/operations.py
@@ -17,14 +17,14 @@ astronomical images.
 """
 
 import logging
-import pickle
 import time
 import warnings
-from multiprocessing import Process, Queue
 
 import astroalign as aa
 
 from astropy.stats import sigma_clipped_stats
+
+from joblib import Parallel, delayed
 
 import numpy as np
 
@@ -36,8 +36,6 @@ import sep
 
 from . import utils as u
 from .single_image import SingleImage as si
-from joblib import Parallel, delayed
-import concurrent.futures
 
 try:
     import pyfftw
@@ -52,6 +50,7 @@ logger = logging.getLogger(__name__)
 
 aa.PIXEL_TOL = 0.5
 eps = np.finfo(np.float64).eps
+OPT_BORDER = 100  # Border size for optimization cost calculation
 
 
 def subtract(
@@ -113,19 +112,19 @@ def subtract(
     if not isinstance(ref, SI):
         try:
             ref = SI(ref, smooth_psf=smooth_psf)
-        except:  # noqa
+        except Exception:
             try:
                 ref = SI(ref.data, smooth_psf=smooth_psf)
-            except:  # noqa
+            except Exception:
                 raise
 
     if not isinstance(new, SI):
         try:
             new = SI(new, smooth_psf=smooth_psf)
-        except:  # noqa
+        except Exception:
             try:
                 new = SI(new.data, smooth_psf=smooth_psf)
-            except:  # noqa
+            except Exception:
                 raise
 
     if align:
@@ -141,7 +140,7 @@ def subtract(
         # new.data = registered
         # new.data.mask = registered.mask
 
-    # make sure that the alignement has delivered arrays of size
+    # make sure that the alignment has delivered arrays of same size
     if new.data.data.shape != ref.data.data.shape:
         raise ValueError("N and R arrays are of different size")
 
@@ -184,13 +183,13 @@ def subtract(
         dx_ref, dy_ref = center_of_mass(p_r)  # [0])
         dx_new, dy_new = center_of_mass(p_n)  # [0])
         if dx_new < 0.0 or dy_new < 0.0:
-            raise ValueError("Imposible to acquire center of PSF inside stamp")
+            raise ValueError("Impossible to acquire center of PSF inside stamp")
 
     psf_ref_hat = _fftwn(p_r, s=ref.data.shape, norm="ortho")
     psf_new_hat = _fftwn(p_n, s=new.data.shape, norm="ortho")
 
-    psf_ref_hat[np.where(psf_ref_hat.real == 0)] = eps
-    psf_new_hat[np.where(psf_new_hat.real == 0)] = eps
+    psf_ref_hat[psf_ref_hat.real == 0] = eps
+    psf_new_hat[psf_new_hat.real == 0] = eps
 
     psf_ref_hat_conj = psf_ref_hat.conj()
     psf_new_hat_conj = psf_new_hat.conj()
@@ -221,12 +220,11 @@ def subtract(
                     - np.roll(gammap, (int(round(dx)), int(round(dy))))
                 )
 
-                border = 100
-                cost = np.ma.MaskedArray(b_n.real, mask=mix_mask, fill_value=0)
-                cost = cost[border:-border, border:-border]
-                cost = np.sum(np.abs(cost / (cost.shape[0] * cost.shape[1])))
+                cost_result = np.ma.MaskedArray(b_n.real, mask=mix_mask, fill_value=0)
+                cost_result = cost_result[OPT_BORDER:-OPT_BORDER, OPT_BORDER:-OPT_BORDER]
+                cost_result = np.sum(np.abs(cost_result / (cost_result.shape[0] * cost_result.shape[1])))
 
-                return cost
+                return cost_result
 
             ti = time.time()
             vec0 = [b, 0.0, 0.0]
@@ -242,10 +240,10 @@ def subtract(
             tf = time.time()
 
             if solv_beta.success:
-                logger.info(("Found that beta = {}".format(solv_beta.x)))
-                logger.info(("Took only {} awesome seconds".format(tf - ti)))
+                logger.info("Found that beta = {}".format(solv_beta.x))
+                logger.info("Took only {} awesome seconds".format(tf - ti))
                 logger.info(
-                    ("The solution was with cost {}".format(solv_beta.cost))
+                    "The solution was with cost {}".format(solv_beta.cost)
                 )
                 b, dx, dy = solv_beta.x
             else:
@@ -255,7 +253,6 @@ def subtract(
                 dx = 0.0
                 dy = 0.0
         elif iterative:  # beta==True & shift==False & iterative==True
-            bi = b
 
             def F(b):
                 gammap = gamma / np.sqrt(new.var**2 + b**2 * ref.var**2)
@@ -280,8 +277,8 @@ def subtract(
 
             tf = time.time()
             if solv_beta.success:
-                logger.info(("Found that beta = {}".format(solv_beta.x)))
-                logger.info(("Took only {} awesome seconds".format(tf - tf)))
+                logger.info("Found that beta = {}".format(solv_beta.x))
+                logger.info("Took only {} awesome seconds".format(tf - ti))
                 b = solv_beta.x
             else:
                 logger.info("Least squares could not find our beta  :(")
@@ -289,7 +286,6 @@ def subtract(
                 b = n_zp / r_zp
             dx = dy = 0.0
         else:  # beta==True & shift==False & iterative==False
-            bi = b
 
             def F(b):
                 gammap = gamma / np.sqrt(new.var**2 + b**2 * ref.var**2)
@@ -304,15 +300,15 @@ def subtract(
 
             ti = time.time()
             solv_beta = optimize.least_squares(
-                F, bi, ftol=1e-8, bounds=[0.1, 10.0], jac="2-point"
+                F, b, ftol=1e-8, bounds=[0.1, 10.0], jac="2-point"
             )
 
             tf = time.time()
             if solv_beta.success:
-                logger.info(("Found that beta = {}".format(solv_beta.x)))
-                logger.info(("Took only {} awesome seconds".format(tf - tf)))
+                logger.info("Found that beta = {}".format(solv_beta.x))
+                logger.info("Took only {} awesome seconds".format(tf - ti))
                 logger.info(
-                    ("The solution was with cost {}".format(solv_beta.cost))
+                    "The solution was with cost {}".format(solv_beta.cost)
                 )
                 b = solv_beta.x
             else:
@@ -322,7 +318,6 @@ def subtract(
             dx = dy = 0.0
     else:
         if shift:  # beta==False & shift==True
-            bi = n_zp / r_zp
             gammap = gamma / np.sqrt(new.var**2 + b**2 * ref.var**2)
             norm = np.sqrt(norm_a + norm_b * b**2)
             dhn = D_hat_n / norm
@@ -335,11 +330,10 @@ def subtract(
                     - _ifftwn(fourier_shift(dhr, (dx, dy)), norm="ortho") * b
                     - np.roll(gammap, (int(round(dx)), int(round(dy))))
                 )
-                border = 100
-                cost = np.ma.MaskedArray(b_n.real, mask=mix_mask, fill_value=0)
-                cost = cost[border:-border, border:-border]
-                cost = np.sum(np.abs(cost / (cost.shape[0] * cost.shape[1])))
-                return cost
+                cost_result = np.ma.MaskedArray(b_n.real, mask=mix_mask, fill_value=0)
+                cost_result = cost_result[OPT_BORDER:-OPT_BORDER, OPT_BORDER:-OPT_BORDER]
+                cost_result = np.sum(np.abs(cost_result / (cost_result.shape[0] * cost_result.shape[1])))
+                return cost_result
 
             ti = time.time()
             vec0 = [0.0, 0.0]
@@ -355,10 +349,10 @@ def subtract(
             tf = time.time()
 
             if solv_beta.success:
-                logger.info(("Found that shift = {}".format(solv_beta.x)))
-                logger.info(("Took only {} awesome seconds".format(tf - ti)))
+                logger.info("Found that shift = {}".format(solv_beta.x))
+                logger.info("Took only {} awesome seconds".format(tf - ti))
                 logger.info(
-                    ("The solution was with cost {}".format(solv_beta.cost))
+                    "The solution was with cost {}".format(solv_beta.cost)
                 )
                 dx, dy = solv_beta.x
             else:
@@ -381,7 +375,7 @@ def subtract(
 
     D = _ifftwn(D_hat, norm="ortho")
     if np.any(np.isnan(D.real)):
-        pass
+        logger.warning("NaN values detected in D.real array after inverse FFT")
 
     d_zp = b / np.sqrt(ref.var**2 * b**2 + new.var**2)
     P_hat = (psf_ref_hat * psf_new_hat * b) / (np.sqrt(norm) * d_zp)
@@ -390,7 +384,7 @@ def subtract(
     dx_p, dy_p = center_of_mass(P)
 
     dx_pk, dy_pk = [val[0] for val in np.where(P == np.max(P))]
-    if (np.abs(dx_p - dx_pk) > 30) or (np.abs(dx_p - dx_pk) > 30):
+    if (np.abs(dx_p - dx_pk) > 30) or (np.abs(dy_p - dy_pk) > 30):
         logger.info("Resetting PSF center of mass to peak")
         dx_p = dx_pk
         dy_p = dy_pk
@@ -422,14 +416,12 @@ def subtract(
     S_corr = _ifftwn(S_hat, norm="ortho") / np.sqrt(V_en + V_er)
     logger.info("S_corr sigma_clipped_stats ")
     logger.info(
-        (
-            "mean = {}, median = {}, std = {}\n".format(
-                *sigma_clipped_stats(S_corr.real.flatten(), sigma=4.0)
-            )
+        "mean = {}, median = {}, std = {}\n".format(
+            *sigma_clipped_stats(S_corr.real.flatten(), sigma=4.0)
         )
     )
     logger.info(
-        ("Subtraction performed in {} seconds\n\n".format(time.time() - t0))
+        "Subtraction performed in {} seconds\n\n".format(time.time() - t0)
     )
 
     return D, P, S_corr.real, mix_mask
@@ -444,124 +436,6 @@ def diff(*args, **kwargs):
         "This is being deprecated in favour of `subtract`", DeprecationWarning
     )
     return subtract(*args, **kwargs)
-
-
-def process_chunk(chunk, global_shape):
-    S_hat = np.zeros(global_shape).astype(np.complex128)
-    psf_hat_sum = np.zeros(global_shape).astype(np.complex128)
-    mix_mask = chunk[0].data.mask
-    for an_img in chunk:
-        np.add(an_img.s_hat_comp, S_hat, out=S_hat, casting="same_kind")
-        np.add(
-            ((an_img.zp / an_img.var) ** 2) * an_img.psf_hat_sqnorm(),
-            psf_hat_sum,
-            out=psf_hat_sum,
-        )
-        mix_mask = np.ma.mask_or(mix_mask, an_img.data.mask)
-    return S_hat.real, S_hat.imag, psf_hat_sum.real, psf_hat_sum.imag, mix_mask
-
-
-def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
-    """Coadd a list of SingleImage instances using R estimator with concurrent.futures.
-
-    Parameters:
-    -----------
-    align : bool
-        Whether to align the images before subtracting, default to False
-    inf_loss : float
-        Value of information loss in PSF estimation, lower limit is 0,
-        upper is 1. Only valid if fitted_psf=False. Default is 0.25
-    n_procs : int
-        Number of parallel workers to use. If value is one then no parallelism
-        is being used. Default 2.
-
-    Returns:
-    --------
-    R : np.ndarray(n, m) of float
-        Coadd image, Zackay's decorrelated R.
-    P : np.ndarray(n, m) of float
-        Coadd image PSF. This is a full PSF image, with a size equal to R
-    mix_mask : np.ndarray of bool
-        Mask of bad pixels for subtracion image, with True marking bad pixels
-    """
-    logger = logging.getLogger()
-    for i_img, animg in enumerate(si_list):
-        if not isinstance(animg, si):
-            si_list[i_img] = si(animg)
-
-    if align:
-        img_list = u._align_for_coadd(si_list)
-        for an_img in img_list:
-            an_img.update_sources()
-    else:
-        img_list = si_list
-
-    shapex = np.min([an_img.data.shape[0] for an_img in img_list])
-    shapey = np.min([an_img.data.shape[1] for an_img in img_list])
-    global_shape = (shapex, shapey)
-
-    zps, meanmags = u.transparency(img_list)
-    for j, an_img in enumerate(img_list):
-        an_img.zp = zps[j]
-        an_img._setup_kl_a_fields(inf_loss)
-
-    psf_shapes = [an_img.stamp_shape[0] for an_img in img_list]
-    psf_shape = np.max(psf_shapes)
-    psf_shape = (psf_shape, psf_shape)
-
-    if n_procs > 1:
-        chunks = list(u.chunk_it(img_list, n_procs))
-        results = []
-        with concurrent.futures.ProcessPoolExecutor(
-            max_workers=n_procs
-        ) as executor:
-            futures = [
-                executor.submit(process_chunk, chunk, global_shape)
-                for chunk in chunks
-            ]
-            for future in concurrent.futures.as_completed(futures):
-                results.append(future.result())
-
-        S_hat = np.zeros(global_shape, dtype=np.complex128)
-        P_hat = np.zeros(global_shape, dtype=np.complex128)
-        mix_mask = np.zeros(global_shape, dtype=np.bool_)
-        # for s_hat_comp, psf_hat_sum, mask in results:
-        for (
-            S_hat_real,
-            S_hat_imag,
-            psf_hat_sum_real,
-            psf_hat_sum_imag,
-            mask,
-        ) in results:
-            np.add(S_hat_real, S_hat.real, out=S_hat.real)
-            np.add(S_hat_imag, S_hat.imag, out=S_hat.imag)
-            np.add(psf_hat_sum_real, P_hat.real, out=P_hat.real)
-            np.add(psf_hat_sum_imag, P_hat.imag, out=P_hat.imag)
-            mix_mask = np.ma.mask_or(mix_mask, mask)
-
-        P_r_hat = np.sqrt(P_hat)
-        P_r = _ifftwn(fourier_shift(P_r_hat, psf_shape))
-        P_r = P_r / np.sum(P_r)
-        R = _ifftwn(S_hat / np.sqrt(P_hat))
-    else:
-        S_hat = np.zeros(global_shape, dtype=np.complex128)
-        P_hat = np.zeros(global_shape, dtype=np.complex128)
-        mix_mask = img_list[0].data.mask
-
-        for an_img in img_list:
-            np.add(an_img.s_hat_comp, S_hat, out=S_hat)
-            np.add(
-                ((an_img.zp / an_img.var) ** 2) * an_img.psf_hat_sqnorm(),
-                P_hat,
-                out=P_hat,
-            )
-            mix_mask = np.ma.mask_or(mix_mask, an_img.data.mask)
-        P_r_hat = np.sqrt(P_hat)
-        P_r = _ifftwn(fourier_shift(P_r_hat, psf_shape))
-        P_r = P_r / np.sum(P_r)
-        R = _ifftwn(S_hat / P_r_hat)
-
-    return R, P_r, mix_mask
 
 
 def process_chunk_joblib(chunk, global_shape):
@@ -591,7 +465,7 @@ def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
     inf_loss : float
         Value of information loss in PSF estimation, lower limit is 0,
         upper is 1. Only valid if fitted_psf=False. Default is 0.25
-    n_jobs : int
+    n_procs : int
         Number of parallel workers to use. If value is one then no parallelism
         is being used. Default 2.
 
@@ -638,7 +512,7 @@ def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
 
         S_hat = np.zeros(global_shape, dtype=np.complex128)
         P_hat = np.zeros(global_shape, dtype=np.complex128)
-        mix_mask = np.zeros(global_shape, dtype=np.bool_)
+        mix_mask = np.zeros(global_shape, dtype=bool)
         for (
             S_hat_real,
             S_hat_imag,
@@ -678,9 +552,9 @@ def coadd(si_list, align=True, inf_loss=0.2, n_procs=2):
 
 
 def stack_R(*args, **kwargs):
-    """Subtract images.
+    """Coadd images.
 
-    Wrapper of `subtract`.
+    Wrapper of `coadd`.
     """
     warnings.warn(
         "This is being deprecated in favour of `coadd`", DeprecationWarning
